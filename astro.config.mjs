160c3646ed75b68293a3bbc0241a22385ad126d2
deleon <holamundo@deleon.agency>
2026-02-17 21:28:37 -0600

    added support for us site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
+  adapter: vercel(),
+  output: 'server',
   site: 'https://taqueritos.com',
   integrations: [
     react(),
